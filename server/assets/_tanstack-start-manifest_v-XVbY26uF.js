1814ce070b454cc1ebea51f90f38013cc0cf94b0
sheisdiniz-pixel <sheisdiniz@gmail.com>
2026-06-08 20:56:00 +0000

    Atualiza bun.lockb

---
+++ server/assets/_tanstack-start-manifest_v-XVbY26uF.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/workspaces/plannerforyounutrivogrouptatianaamorim/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-VP0M1Uw0.js"] }, "/": { filePath: "/workspaces/plannerforyounutrivogrouptatianaamorim/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-C3QxK-Rs.js"] } }, clientEntry: "/assets/index-VP0M1Uw0.js" });
+export {
+  tsrStartManifest
+};
